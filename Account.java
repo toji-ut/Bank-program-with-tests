b40139f9a434cb0a0a9ad90b3e8f3a3e09d52721
toji-ut <107822013+toji-ut@users.noreply.github.com>
2023-04-04 15:27:42 -0400

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,4 +1,4 @@
-public class Account {
+public class Account implements Comparable{
 
     private String name;
     private String id;
@@ -45,4 +45,24 @@ public class Account {
     public boolean equals(Account other) {
         return (this.name.equals(other.name) && this.id.equals(other.id) && this.balance.equals(other.balance));
     }
+
+    // compareTo: compare two Account objects.
+    // Precondition: parameter o is an Object (of type Account)
+    // Postcondition: return 0 if this.id is the same as o.id, -1 if this.id < o.id, 1 if this.id > o.id.
+    @Override
+    public int compareTo(Object o) {
+        if (o instanceof Account) {
+            if (this.id.equals(((Account) o).id)){
+                return 0;
+            } else if (this.id.compareTo(((Account) o).id) < 0) {
+                return -1;
+            } else {
+                return 1;
+            }
+        }
+        else {
+            throw new ClassCastException("Object is not an instance of Account");
+        }
+    }
+
 }
