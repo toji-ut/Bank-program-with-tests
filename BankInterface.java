65382021b821ff58150222ed4604b0e64bba5d5a
toji-ut <107822013+toji-ut@users.noreply.github.com>
2023-04-04 15:29:14 -0400

    Add files via upload

---
+++ BankInterface.java
@@ -0,0 +1,14 @@
+public interface BankInterface {
+    public void addAccount(Account account);
+
+    public Account search(String id);
+
+    public void deposit(String id, Money amount);
+
+    public void withdraw(String id, Money amount);
+
+    public void sortAccounts();
+    public String toString();
+
+    public int getNumOfAccounts();
+}
