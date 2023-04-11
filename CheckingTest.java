2f77893aebbb520e37cbd65f423253ad57f99cc7
toji-ut <107822013+toji-ut@users.noreply.github.com>
2023-04-11 13:35:33 -0400

    Update CheckingTest.java

--- CheckingTest.java
+++ CheckingTest.java
@@ -18,9 +18,23 @@ public class CheckingTest {
     }
 
     @Test
-    public void testWithdrawOutsideOverdraft() {
+    public void testOverWithdraw() {
+        // Create a checking account.
         Checking checking = new Checking("John Doe", "1234", new Money(100, 0), new Money(50, 0));
-        checking.withdraw(new Money(200, 0));
-        assertTrue(checking.getBalance().equals(new Money(100, 0)));
+
+        try {
+            // Withdraw an amount that should cause an exception of type InsufficientFundsException...
+            checking.withdraw( new Money(200, 0) );
+
+            // If we reach this point in the code, that means the exception was not thrown as expected, so this test case fails.
+            fail();
+        }
+        catch (InsufficientFundsException ife) {
+            System.out.println("InsufficientFunds Exception on testOverWithdraw");
+            ife.printStackTrace();
+        }
+
     }
+
+
 }
