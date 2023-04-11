2d8135677a044cba6d73e0e532c6566fcef64e44
toji-ut <107822013+toji-ut@users.noreply.github.com>
2023-04-11 13:35:14 -0400

    Add files via upload

---
+++ InsufficientFundsException.java
@@ -0,0 +1,7 @@
+class InsufficientFundsException extends RuntimeException {
+    public InsufficientFundsException(String s) {
+        super(s);
+    } // end constructor
+
+    // All other methods are inherited.
+} // end MyRuntimeException
