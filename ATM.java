4ab0ae8f9dfdc645d21b39223d0e09b2af5c2ee5
toji-ut <107822013+toji-ut@users.noreply.github.com>
2023-04-27 19:23:35 -0400

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -1,51 +1,109 @@
+/**
+ * ATM class represents a driver program for Bank project.
+ * It allows users to perform transactions such as check balance, deposit, and withdraw money from their account.
+ * Users are prompted to enter their ID and transaction type until they choose the fourth option to quit the program.
+ * The program reads data from a file containing account information and writes the output to another file.
+ */
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.FileWriter;
-
-// import static org.junit.Assert.assertEquals;
-
-// Driver class for Bank project
-
 public class ATM {
+    /**
+     * Main method of the program.
+     * @param args Command-line arguments.
+     */
     public static void main(String[] args) {
         try {
-
             // Read data from a file into a Bank.
             // Each line of the file has info for one account.
             BankInterface bank = readFromFile("accounts_list.txt");
-
             bank.sortAccounts();
 
             // Print all the data stored in the bank.
             System.out.println(bank);
 
             IOHandlerInterface ioh = new IOHandlerStandard();
-            // Read user input.
-            String id = readUserID(ioh);
-            // Echo user input.
-            ioh.put("You entered: " + id);
+            String id, transactionType;
 
-            if (isValid(bank, id)) {
-                System.out.println("Account FOUND for ID: " + id);
-                System.out.println(bank.search(id));
-            } else {
-                System.out.println("Account NOT FOUND for ID: " + id);
-            }
+            // Read user input.
+            do {
+                id = readUserID(ioh);
+                if (id.equalsIgnoreCase("quit")) {
+                    ioh.put("Thank you for using our ATM. Goodbye!");
+                    System.exit(0);
+                } else if (isValid(bank, id)) {
+                    ioh.put("Account FOUND for ID: " + id);
+                    ioh.put(bank.search(id).toString());
+                    break;
+                } else {
+                    ioh.put("Account NOT FOUND for ID: " + id);
+                }
+            } while (true);
+
+            // Prompt user to choose a transaction type until they choose to quit.
+            label:
+            do {
+                // Read user input.
+                transactionType = ioh.get("Please enter a transaction type (check balance (1) / deposit (2) / withdraw (3) / quit (4)): ");
+
+                // Normalize transaction type to lowercase.
+                transactionType = transactionType.toLowerCase();
+
+                // Check balance.
+                switch (transactionType) {
+                    case "1": {
+                        ioh.put(bank.search(id).toString());
+                        break;
+                    }
+
+                    // Deposit.
+                    case "2": {
+                        Account account = bank.search(id);
+                        Money amount = readMoney(ioh);
+                        account.deposit(amount);
+                        ioh.put("Deposit successful. New balance for account (" + account.getId() + "): " + account.getBalance());
+                        break;
+                    }
+
+                    // Withdraw.
+                    case "3": {
+                        Account account = bank.search(id);
+                        Money amount = readMoney(ioh);
+                        if (account.getBalance().compareTo(amount) >= 0) {
+                            account.withdraw(amount);
+                            ioh.put("Withdrawal successful. New balance for account (" + account.getId() + "): " + account.getBalance());
+                        } else {
+                            ioh.put("Withdrawal failed. Insufficient funds in account (" + account.getId() + ").");
+                        }
+                        break;
+                    }
+
+                    // Quit.
+                    case "4":
+                        ioh.put("Thank you for using our ATM. Goodbye!");
+                        break label;
+
+
+                    // Invalid transaction type.
+                    default:
+                        ioh.put("Invalid transaction type: " + transactionType);
+                        break;
+                }
+            } while (true);
 
             writeToFile("output_list.txt", bank);
-        } // end try
-        catch (IOException ioe) {
+        } catch (IOException ioe) {
             System.out.println("IOException in main: " + ioe.getMessage());
             ioe.printStackTrace();
-        } // end catch
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("Exception in main: " + e.getMessage());
+
             e.printStackTrace();
         } // end catch
 
-    } // end main
+    }   // end main
 
     /**
      * readFromFile: Read the accounts from a text file
@@ -64,12 +122,8 @@ public class ATM {
 
             // Store the account info in the bank.
             bank.addAccount(account);
-
         } // end while
-
-
         return bank;
-
     } // end readFromFile
 
     /**
@@ -79,11 +133,32 @@ public class ATM {
      * @return String id read from user.
      */
     public static String readUserID(IOHandlerInterface ioh) {
-        return ioh.get("Please enter your ID: ");
+        return ioh.get("Please enter your ID or 'quit': ");
     }
 
     public static boolean isValid(BankInterface bank, String id) {
         return bank.search(id) != null;
+    }
+
+    public static Money readMoney(IOHandlerInterface ioh) {
+        String input = ioh.get("Please enter the amount to deposit (in the format x.xx): ");
+        int dollars = 0, cents = 0;
+        try {
+            String[] parts = input.split("\\.");
+            if (parts.length == 2) {
+                dollars = Integer.parseInt(parts[0]);
+                cents = Integer.parseInt(parts[1]);
+                if (cents >= 100) {
+                    throw new NumberFormatException();
+                }
+            } else {
+                throw new NumberFormatException();
+            }
+        } catch (NumberFormatException e) {
+            ioh.put("Invalid input. Please try again.");
+            return readMoney(ioh);
+        }
+        return new Money(dollars, cents);
     }
 
     /**
