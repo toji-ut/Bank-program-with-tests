67fdb91b29c8a84d66e4a252c0b43cb20854d571
toji-ut <107822013+toji-ut@users.noreply.github.com>
2023-04-28 22:43:02 -0400

    Update InputManager.java

--- InputManager.java
+++ InputManager.java
@@ -1,42 +1,54 @@
-
 /**
  * Manage input to be read from either keyboard or file.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @version 1.0
  */
-
 import java.util.Scanner;
 
-public class InputManager
-{
-    // Method: readOneAccountFrom
-    // Precondition: inputSource is a Scanner object, already set up
-    // to read from a text file or standard input source (keyboard).
-    // Postcondition: returns an Account with the data read for its attributes.
-    // Assumption: Account data will be in the format of: name,id,balance
-    public static Account readOneAccountFrom (Scanner inputSource)
-    {
+public class InputManager {
+    /**
+     * Reads account data from a Scanner object and returns an Account object.
+     * Assumes account data is in the format of: id, name, balance, account type (r = regular, c = checking),
+     * and overdraft maximum (if account type is checking).
+     *
+     * @param inputSource a Scanner object set up to read from a text file or standard input source (keyboard).
+     * @return an Account object with the data read for one account.
+     */
+    public static Account readOneAccountFrom(Scanner inputSource) {
         // Read one line of account data into oneLine
-        System.out.println ("Reading: name, id, balance...");
+        System.out.println("Reading: id, name, balance, account type, overdraft maximum...");
         String oneLine = inputSource.nextLine();
 
-        // Parse line of account data, separated by commas.
-        Scanner lineTokenizer = new Scanner (oneLine);
-        lineTokenizer.useDelimiter (", ");
+        // Parse line of account data, separated by commas
+        Scanner lineTokenizer = new Scanner(oneLine);
+        lineTokenizer.useDelimiter(", ");
 
-        // Get account data (i.e. name, accountNum and balance) from oneLine
-        String name = lineTokenizer.next ();
-        String accountId = lineTokenizer.next ();
-        int dollars = lineTokenizer.nextInt ();
-        int cents = lineTokenizer.nextInt ();
-        Money AccBalance = new Money(dollars, cents);
+        // Get account data (i.e. name, id, balance, accountType, and overdraftMax) from oneLine
+        String name = lineTokenizer.next();
+        String accountId = lineTokenizer.next();
+        int dollars = lineTokenizer.nextInt();
+        int cents = lineTokenizer.nextInt();
+        Money balance = new Money(dollars, cents);
+        String accountType = lineTokenizer.next();
+        Money overdraftMax = null;
+        if (accountType.equals("c")) {
+            dollars = lineTokenizer.nextInt();
+            cents = lineTokenizer.nextInt();
+            overdraftMax = new Money(dollars, cents);
+        }
 
-        // Create and return an Account object with the data read for one   account.
-        Account oneAccount = new Account (name, accountId, AccBalance);
-        System.out.println ("Account read: " + oneAccount + "\n");
-
-        return oneAccount;
+        // Create and return an Account object with the data read for one account
+        if (accountType.equals("r")) {
+            Account oneAccount = new Account(name, accountId, balance);
+            System.out.println("Account read: " + oneAccount + "\n");
+            return oneAccount;
+        } else if (accountType.equals("c")) {
+            Checking oneAccount = new Checking(name, accountId, balance, overdraftMax);
+            System.out.println("Account read: " + oneAccount + "\n");
+            return oneAccount;
+        } else {
+            System.out.println("Invalid account type: " + accountType);
+            return null;
+        }
     } // end readOneAccountFrom
-
 } // end InputManager
