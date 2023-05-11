2fb7e2b83b88db8a714ae797265ed235d9743fd1
toji-ut <107822013+toji-ut@users.noreply.github.com>
2023-05-11 11:49:22 -0400

    Update Bank2.java

--- Bank2.java
+++ Bank2.java
@@ -18,7 +18,7 @@ public class Bank2 implements BankInterface{
     }
 
     public Account search(String id) {
-        int index = BinarySearchWithArrayList.binarySearchWithArrayList(accounts, 0, accounts.size()-1, id);
+        int index = binarySearchWithArrayList(accounts, 0, accounts.size()-1, id);
         if (index == -1) {
             return null;
         } else {
@@ -60,4 +60,48 @@ public class Bank2 implements BankInterface{
         return nameOfBank;
     }
 
+    public int binarySearchWithArrayList(ArrayList<Account> anArray, int first, int last, String value) {
+
+    // Searches the array items anArray[first] through
+    // anArray[last] for value by using a binary search.
+    // Precondition: 0 <= first, last <= SIZE-1, where
+    // SIZE is the maximum size of the array, and
+    // anArray[first] <= anArray[first+1] <= ... <= anArray[last].
+    // Postcondition: If value is in the array, the method
+    // returns the index of the array item that equals value;
+    // otherwise the method returns -1.
+
+        int index;
+
+        if (first > last) {
+
+            index = -1;      // value not in original array
+
+        }
+        else {
+
+            // Invariant: If value is in anArray,
+            // anArray[first] <= value <= anArray[last]
+
+            int mid = (first + last)/2;
+
+            if (value.equals(anArray.get(mid).getId())) {
+
+                index = mid;  // value found at anArray[mid]
+
+            }
+            else if (value.compareTo(anArray.get(mid).getId()) < 0) {
+
+                index = binarySearchWithArrayList(anArray, first, mid-1, value);   // point X
+
+            }
+            else {
+
+                index = binarySearchWithArrayList(anArray, mid+1, last, value);    // point Y
+
+            }  // end if
+        }  // end if
+
+        return index;
+    }  // end binarySearch
 }
